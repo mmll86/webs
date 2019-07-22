ecad244c028b7082aac95686189018c6bb0a16e1
mmll86 <50227293+mmll86@users.noreply.github.com>
2019-07-22 20:56:54 +0900

    new project

--- evroclassik/models.py
+++ evroclassik/models.py
@@ -3,18 +3,19 @@ from django.db import models
 
 class Catalog(models.Model):
 	text = models.CharField(max_length=200, blank=True)
-	#image = models.ImageField(upload_to='media')
+	# image = models.ImageField(upload_to='media')
 	date_added = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return self.text
 
+
 class Furniture(models.Model):
-	name =models.ForeignKey('Catalog', on_delete = models. PROTECT)
+	name = models.ForeignKey('Catalog', on_delete=models.PROTECT)
 	name_furniture = models.CharField(max_length=50, blank=True)
-	#image = models.ImageField(upload_to='media')
+	# image = models.ImageField(upload_to='media')
 	price = models.IntegerField()
-	text =models.TextField()
+	text = models.TextField()
 	date_added = models.DateTimeField(auto_now_add=True)
 
 	class Meta:
@@ -32,12 +33,13 @@ class CatalogPlumbing(models.Model):
 	def __str__(self):
 		return self.text
 
-class Plumbing (models.Model):
-	name =models.ForeignKey('CatalogPlumbing', on_delete = models. PROTECT)
+
+class Plumbing(models.Model):
+	name = models.ForeignKey('CatalogPlumbing', on_delete=models.PROTECT)
 	name_plumbing = models.CharField(max_length=50, blank=True)
-	#image = models.ImageField(upload_to='media')
+	# image = models.ImageField(upload_to='media')
 	price = models.IntegerField()
-	text =models.TextField()
+	text = models.TextField()
 	date_added = models.DateTimeField(auto_now_add=True)
 
 	class Meta:
