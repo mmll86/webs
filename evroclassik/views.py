1500b9f735978abae6fec12e707f6f159b83f54b
root <mmll86@bk.ru>
2019-07-26 12:29:31 +0900

    нужно распарсить view на base
    Не отображается новый класс
    
    поправить view base

--- evroclassik/views.py
+++ evroclassik/views.py
@@ -1,36 +1,40 @@
 from django.shortcuts import render
-from .models import Catalog, CatalogPlumbing
+from .models import Catalog, CatalogPlumbing, Slider
 
 
 def index(request):
-	return render(request, "evroclassik/index.html")
+    return render(request, "evroclassik/index.html")
 
-#Каталог мебели
+
+# Каталог мебели
 def catalogs(request):
-	catalogs = Catalog.objects.order_by('date_added')
-	context = {'catalogs': catalogs}
-	return render(request, 'evroclassik/catalogs.html', context)
+    catalogs = Catalog.objects.order_by('date_added')
+    context = {'catalogs': catalogs}
+    return render(request, 'evroclassik/catalogs.html', context)
 
 
 def catalog(request, catalog_id):
-	catalog = Catalog.objects.get(id=catalog_id)
-	furnitures = catalog.furniture_set.order_by('-date_added')
-	context = {'catalog': catalog, 'furnitures': furnitures}
-	return render(request, 'evroclassik/catalog.html', context)
+    catalog = Catalog.objects.get(id=catalog_id)
+    furnitures = catalog.furniture_set.order_by('-date_added')
+    context = {'catalog': catalog, 'furnitures': furnitures}
+    return render(request, 'evroclassik/catalog.html', context)
 
-#Каталог сантехники
+
+# Каталог сантехники
 def catalogs_plumbing(request):
-	catalogs_plumbing = CatalogPlumbing.objects.order_by('date_added')
-	context = {'catalogs_plumbing': catalogs_plumbing}
-	return render(request, 'evroclassik/catalogs_plumbing.html', context)
+    catalogs_plumbing = CatalogPlumbing.objects.order_by('date_added')
+    context = {'catalogs_plumbing': catalogs_plumbing}
+    return render(request, 'evroclassik/catalogs_plumbing.html', context)
 
 
 def plumbing(request, plumbing_id):
-	plumbing = CatalogPlumbing.objects.get(id=plumbing_id)
-	plumbings= plumbing.plumbing_set.order_by('-date_added')
-	context = {'plumbing': plumbing, 'plumbings': plumbings }
-	return render(request, 'evroclassik/plumbing.html', context)
+    plumbing = CatalogPlumbing.objects.get(id=plumbing_id)
+    plumbings = plumbing.plumbing_set.order_by('-date_added')
+    context = {'plumbing': plumbing, 'plumbings': plumbings}
+    return render(request, 'evroclassik/plumbing.html', context)
 
 
-
-
+def base(request):
+    sliders = Slider.objects.order_by('text')
+    context = {'sliders': sliders}
+    return render(request, 'evroclassik/base.html', context)
